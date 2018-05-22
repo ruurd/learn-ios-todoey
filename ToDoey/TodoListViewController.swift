49fbae328c804d8576c5c5e5fefd6bf14aa751be
Ruurd Pels <rfpels@gmail.com>
2018-05-22 18:23:45 +0200

    Clean up application, use constants

--- ToDoey/TodoListViewController.swift
+++ ToDoey/TodoListViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let DEFAULTSKEY = "TodoListArray"
+    let TODOITEMCELL = "TodoItemCell"
+
     let defaults = UserDefaults.standard
 
     var itemArray = [String]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "TodoListArray") as! [String] {
+        if let items = defaults.array(forKey: DEFAULTSKEY) as! [String] {
             itemArray = items
         }
     }
@@ -30,11 +33,10 @@ class TodoListViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: TODOITEMCELL, for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
-
 
     // MARK: TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -46,7 +48,7 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
-    // MARK: Add new item
+    // MARK: Add a new item to the array and persist
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField() // Pull the text field in the alert out of its scope so we can access it.
 
@@ -56,7 +58,7 @@ class TodoListViewController: UITableViewController {
             print(textField.text!)
             if let t = textField.text {
                 self.itemArray.append(t)
-                self.defaults.set(self.itemArray, forKey: "TodoListArray")
+                self.defaults.set(self.itemArray, forKey: DEFAULTSKEY)
                 self.tableView.reloadData()
             }
         }
